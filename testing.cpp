f4b3032453bbbaccc58e49b09e098676ffac2a1d
Noah <Snowbear0408@gmail.com>
2020-04-17 23:32:11 +0100

    testing.cpp hot fix
    Fixed some bugs in the file so it would run

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,9 @@
-int main()
-{
-    std::cout << "Testing  C++ files"
-    std::cout << "Snowb"
+#include <iostream>
+
+int main() {
+    std::cout << "Testing  C++ files\n";
+    std::cout << "Snowb\n";
 }
+
+//Dis is some code that I made to learn some really basic stuff. Not important so you can remove if needed I think.
+//I assume everything will get wiped when we start the project for real though tbh. 
