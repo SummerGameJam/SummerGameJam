2b9862e66de81f801ed30038d14847b06cac9e8c
Noah <Snowbear0408@gmail.com>
2020-04-18 02:18:44 +0100

    offfice 1.0 update
    No clue what this was, unsaved change though.

--- Office 1.0.cpp
+++ Office 1.0.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <string>
+
+std::string Car() {
+  std::string car;
+  std::cout << "Do you have a car?\n";
+  std::cin >> car;
+  return car;
+};
+
+std::string Office() {
+  std::string office;
+  std::cout << "Do you want an office?\n";
+  std::cin >> office;
+  if (office == "yes") {
+    return "yes";
+  } 
+  else {
+    std::cout << "Don't buy an office\n";
+    return "no";
+  };
+};
+
+void havecar() {
+  std::string car;
+  car = Car();
+
+  if (car == "yes"){
+    std::cout << "Buy an office";
+  }
+  else if (car == "no") {
+    std::cout << "You need to buy a car first";
+  };
+};
+
+std::string Money() {
+  std::string money;
+  std::cout << "Do you have money?\n";
+  std::cin >> money;
+
+  return money;
+  };
 
 int main() {
-    std::cout << "Testing  C++ files\n";
-    std::cout << "Snowb\n";
-}
 
-//Dis is some code that I made to learn some really basic stuff. Not important so you can remove if needed I think.
-//I assume everything will get wiped when we start the project for real though tbh. 
+  std::string money;
+  std::string office;
+  money = Money();
+
+  if ((money == "yes") || (money == "Yes")){
+      office = Office();
+    if (office == "yes"){
+      havecar();
+    };
+  };
+
+  return 0;
+};
+
+
+//thanks pixie :)
+//written at two in morning. - IK its shit ;) 
