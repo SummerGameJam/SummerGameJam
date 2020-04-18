319bb275eba0a8f8f6eb7e801ec5dac009ebf2ac
Noah <Snowbear0408@gmail.com>
2020-04-18 11:17:19 +0100

    Start of hangman game
    Saving the start of the hangman game

---
+++ Hangman V1/Game.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <time.h>
+using namespace std;
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+    if (printTop)
+    {
+        cout << "+---------------------------------+" << endl;
+        cout << "|";
+    }
+    else
+    {
+        cout << "|";
+    }
+    bool front = true;
+    for (int i = message.length(); i < 33; i++)
+    {
+        if (front)
+        {
+            message = " " + message;
+        }
+        else
+        {
+            message = message + " ";
+        }
+        front = !front;
+    }
+    cout << message.c_str();
+ 
+    if (printBottom)
+    {
+        cout << "|" << endl;
+        cout << "+---------------------------------+" << endl;
+    }
+    else
+    {
+        cout << "|" << endl;
+    }
+}
+int main()
+{
+    PrintMessage("HANG MAN");
+    getchar();
+    return 0;
+}
+
+void DrawMan(int guesCount = 0)
+{
+    if(guessCount >=1)
+        PrintMessage("|", false, false);
+    else
+    {
+        PrintMessage("", false, false);
+        //The line above is need so we have a blank line to call 
+    }
+    
+}
+
+/*
++---------------------------------+
+|             HANG MAN            |
++---------------------------------+
+|               |                 |
+|               |                 |
+|               O                 |
+|              /|\                |
+|               |                 |
+|              / \                |
+|         +----------+            |
+|         |          |            |
++---------------------------------+
+|        Available letters        |
++---------------------------------+
+|     A B C D E F G H I J K L M   |
+|     N O P Q R S T U V W X Y Z   |
++---------------------------------+
+|         Guess the word          |
++---------------------------------+
+| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |
++---------------------------------+
+>
+*/
