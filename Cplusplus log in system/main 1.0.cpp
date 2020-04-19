2eb0335886d788bae3fd91aeabd5fb36f4ea9c7b
Noah <Snowbear0408@gmail.com>
2020-04-19 18:28:11 +0100

    C++ log in sytem finished
    Ill add more features to it later but for now it works :)

--- Cplusplus log in system/main 1.0.cpp
+++ Cplusplus log in system/main 1.0.cpp
@@ -12,13 +12,22 @@ int main() {
 
     bool correct = false;
 
+    std::ifstream ifile;
+
     std::string login1;
     std::string login2;
+		std::string data;
+		
+		bool real = true;
+
+    int x;
+    int sum;
+    sum = 0;
 
     std::cout << "Password sytem\n";
 
     std::cout << "Do you have an account already? y/n: ";
-    std::cin >> welcome;
+    std::cin >> welcome; 
     if (welcome == "n" ) {
         while (correct == false) {
           std::cout << "Enter a username: ";
@@ -31,10 +40,10 @@ int main() {
           std::cin >> password1;
 
           if (password == password1) {
-            std::ofstream myfile;
-            myfile.open (username + ".txt");
-            myfile << (username + ":" + password);
-            myfile.close();
+            std::ofstream ofile;
+            ofile.open (username + ".txt");
+            ofile << (username + ":" + password);
+            ofile.close();
             correct = true;
           }
           else {
@@ -42,4 +51,24 @@ int main() {
         };
     };
   };
+    if (welcome == "y") {
+        
+        while (real == true) {
+            std::cout << "Please enter your username: \n";
+            std::cin >> login1;
+            std::cout << "Please enter your password: \n";
+            std::cin >> login2;
+            ifile.open (login1 + ".txt");
+            ifile >> data;
+            if (ifile || data == login1 + ":" + login2){
+              std::cout << "Welcome back\n";
+              ifile.close();
+              real = false;
+            }
+            else {
+                std::cout << "Your username or password is invalid, please try again\n";
+            };
+        };
+    };
+    std::cout << "Testing 123";
 };
