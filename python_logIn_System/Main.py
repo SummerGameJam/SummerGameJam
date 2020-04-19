c133553eca8d27391ca88fa0b6a8a91fcf830c44
Noah <Snowbear0408@gmail.com>
2020-04-19 14:38:10 +0100

    Python log in fix
    Fixed some bugs, should work now

--- python_logIn_System/Main.py
+++ python_logIn_System/Main.py
@@ -1,8 +1,33 @@
+print("Login system")
 welcome = input("Do you have an account already? y/n: ")
 if welcome == "n":
-    While True:
-    username = input("Enter a username: ")
-    password = input("Enter a password: ")
-    password1 = input("Enter your password again: ")
-    if password == password1:
-        file = open(username + ".txt", "w")
+    while True:
+        username = input("Enter a username: ")
+        password = input("Enter a password: ")
+        password1 = input("Please confirm your password: ")
+        if password == password1:
+            file = open(username + ".txt", "w")
+            file.write(username + ":" + password)
+            file.close()
+            welcome = "y"
+            break
+        print(
+            "We are sorry but your passwords do not match, please try again! - If you need assistance please contact the admin"
+        )
+#^ if you do not have an account 
+#Bellow if you do have an account
+
+if welcome == "y":
+    while True:
+        print("Welcome back!")
+        login1 = input("Login: ")
+        login2 = input("Password: ")
+        file = open(login1 + ".txt", "r")
+        data = file.readline()
+        file.close()
+        if data == login1 + ":" + login2:
+            print("Welcome back")
+            break
+        print(
+            "Your username or password is invalid, please try again"
+        )
