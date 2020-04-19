00c4d6c71489345573567e0c8513c90e77334c3c
Noah <Snowbear0408@gmail.com>
2020-04-19 16:53:37 +0100

    C++ fix
    Bug fix, need to rest later

--- Cplusplus log in system/main 1.0.cpp
+++ Cplusplus log in system/main 1.0.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string> 
+#include <fstream>
 
-int main(){
+int main() {
 
     std::string welcome;
     
     std::string username;
     std::string password;
     std::string password1;
+
+    bool correct = false;
 
     std::string login1;
     std::string login2;
@@ -17,23 +20,26 @@ int main(){
     std::cout << "Do you have an account already? y/n: ";
     std::cin >> welcome;
     if (welcome == "n" ) {
-        while true;
-            std::cout << "Enter a username: ";
-            std::cin >> username;
+        while (correct == false) {
+          std::cout << "Enter a username: ";
+          std::cin >> username;
 
-            std::cout << "Enter a password: ";
-            std::cin >> password;
+          std::cout << "Enter a password: ";
+          std::cin >> password;
 
-            std::cout << "Enter your pasword again: ";
-            std::cin >> password1;
+          std::cout << "Enter your password again: ";
+          std::cin >> password1;
 
-            if (password == password1);
-                ofstream myfile;
-                myfile.open (username + ".txt");
-                myfile << (username + ":" password);
-                myfile.close();
-                welcome = false;
-            std::cout << "Your passwords do not match, please try again";
-    }
-
-}
+          if (password == password1) {
+            std::ofstream myfile;
+            myfile.open (username + ".txt");
+            myfile << (username + ":" + password);
+            myfile.close();
+            correct = true;
+          }
+          else {
+            std::cout << "Your passwords do not match, please try again\n";
+        };
+    };
+  };
+};
