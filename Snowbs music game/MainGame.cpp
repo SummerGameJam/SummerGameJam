8fa07804f68fe98d267c6a163b65b56c2fbff41c
Noah <Snowbear0408@gmail.com>
2020-04-21 22:44:47 +0100

    Added a bunch of shit
    Added loads of shit, it runs but does not work logicaly right now. Not done yet.

--- Snowbs music game/MainGame.cpp
+++ Snowbs music game/MainGame.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string> 
-#include <fstream>
+#include <fstream> //for file stuff
 
-void SongType() {
-    if (SongType == "A") {
-std::ifstream RockSongs;
-    };
+void SongTypeFunc(std::string songType) {
 
-    if (SongType == "B") {
-std::ifstream PopSongs;
+    if (songType == "A") { 
+      std::ifstream RockSongs;
     }
-}
+
+    if (songType == "B") {
+    	std::ifstream PopSongs;
+    }
+};
 
 int main() {
-
+		/*
+		all log in sytem variables are defined here for some reason
+		dunno what i was thinking 
+		but cba changing, maybe later
+		*/
     std::string welcome;
     
     std::string username;
@@ -58,9 +63,14 @@ int main() {
           }
           else {
             std::cout << "Your passwords do not match, please try again\n";
-        };
-    };
-  };
+						correct = false;
+        }
+    }
+  }
+		else {
+			std::cout << "Please only enter a lowercase 'y' or 'n', the proggram will now close";
+			std::terminate();
+		};
     if (welcome == "y") {
         
         while (real == true) {
@@ -76,10 +86,14 @@ int main() {
               real = false;
             }
             else {
-                std::cout << "Your username or password is invalid, please try again\n";
+                std::cout << "Your username or password is invalid, please try again\n";	
             };
         };
-    };
+    }
+		else {
+			std::cout << "Please only enter a lowercase 'y' or 'n', the proggram will now close";
+			std::terminate();
+		};
 
     std::cout << "Welcome!\n";
     std::string gameinfo;
@@ -88,35 +102,114 @@ int main() {
 
     if (gameinfo == "n") {
         std::cout << "Ok, we are here when you are ready :)\n";
-				//call function to exit here
+				std::terminate();
 		}
     else {
-        std::string "Lets go and have some fun :)\n";
-    };
+        std::cout << "Lets go and have some fun :)\n";
+    }
 
     std::string instructions;
     std::cout << "Do you want to hear the instructions? y/n: \n";
     std::cin >> instructions;
     if (instructions == "y") {
-        std::string "...Thease are the instructions...";
+        std::cout << "...Thease are the instructions...\n";
 		}
     else {
-        std::string "Thats fine, we shall continue";
-    };
-
-    std::cout << "You can pick what type of songs you wish to be played";
-    std::cout << "A = Rock Songs";
-    std::cout << "B = Pop Songs";
-    std::string SongType;
-    std::cout << "Pleae enter what group you want - A,B...: \n";
-    std::cin >> SongType;
-
-		//call the song type function 
-    counter = 0
-    points = 0
-    while (counter < 5) {
-		//call the song type function 
-        //Play a random song from the folder with the correct song type 
+        std::cout << "Thats fine, we shall continue\n";
     }
 
+    std::cout << "You can pick what type of songs you wish to be played\n";
+    std::cout << "A = Rock Songs\n";
+    std::cout << "B = Pop Songs\n";
+    std::string songType;
+    std::cout << "Please enter what group you want - A,B...: \n";
+    std::cin >> songType;
+
+		SongTypeFunc(songType);
+
+    int counter;
+    int points;
+		int lifes;
+
+    std::string SoundLocation;
+
+    counter = 0;
+    points = 0; 
+		lifes = 4;
+
+    while (counter < 5) { 
+		  
+			SongTypeFunc(songType);
+      counter += 1; //increase counter by one
+			
+
+			if (songType == "A") { 
+      	SoundLocation = "E:\Documents\GitHub\SummerGameJam\Snowbs music game\Files\Audio\RockSongs"; //file directory of RockSongs folder;
+    	}
+
+    	if (songType == "B") {
+    		SoundLocation = "E:\Documents\GitHub\SummerGameJam\Snowbs music game\Files\Audio\PopSongs"; //file directory of PopSongs
+    	}
+
+			std::string PlayedSong;
+      PlayedSong = "Hello"; //this needs to = the song that was played
+
+			std::string ThoughtPlayedSong;
+			std::cout << "What song do you think was played?: \n";
+			std::cin >> ThoughtPlayedSong;
+
+			std::cout << "The list of songs were\n";
+
+			SongTypeFunc(songType);
+
+			if ((PlayedSong == ThoughtPlayedSong) && (counter == 1)) {
+				points=points+3;
+				std::cout << "Well done, you have got three points.\n";
+			}
+			else {
+				lifes=lifes-1;
+				std::cout << "Not quite right, try again.\n";
+				std::cout << "You have, " << lifes << " left";
+      }
+
+			if ((PlayedSong == ThoughtPlayedSong) && (counter == 2)) {
+				points=points+2;
+				std::cout << "Well done, you have got two points.";
+			}
+			else {
+				lifes=lifes-1;
+				std::cout << "Not quite right, try again.\n";
+				std::cout << "You have, " << lifes << " left";
+			}
+
+			if ((PlayedSong == ThoughtPlayedSong) && (counter == 3)) {
+				points=points+1;
+				std::cout << "Well done, you have got one point.";
+			}
+			else {
+				lifes=lifes-1;
+				std::cout << "Not quite right, try again.\n";
+				std::cout << "You have, " << lifes << " left";
+			}
+
+			//loop back to start of while statment
+
+			//global statment so if at any point lives = 0 it shows the score board function 
+
+			//global statment so if at end point user types STOP the game quits and the quit stuff is shown (Scorebord funtino would be called in that)
+
+			//should write you values to your acount file so you can continue playing once you have logged in. This will prob be in versino 2.0
+
+			/*
+			make the directory a variable
+		 	make a new function inside the main one 
+			like if songType is A then VARIABLENAME == rocksongs 
+			
+			(folder)
+			then do the same thing for B and other ones
+			then assing the song name to a variable
+			and if the user input = that variable then points and so on
+			*/
+    }
+	return 0;
 };
