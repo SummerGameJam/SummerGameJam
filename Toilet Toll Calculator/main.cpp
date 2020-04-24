c59b01236501cd023c325b61435da4d1ca70feda
Noah <Snowbear0408@gmail.com>
2020-04-25 00:47:21 +0100

    IDEK
    I was board ok... 
    
    Not even done / will not ever be done

---
+++ Toilet Toll Calculator/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() 
+{
+	std::cout << "Welcome to the Toilet Roll Calculator, this will tell you how much toilet roll you should buy!" << std::endl;
+	std::cout << "Press enter to continue";
+	std::cin.get();
+
+	std::string AmountOfPeople;
+	std::cout << "How many people are in your house?: ";
+	std::cin >> AmountOfPeople;
+
+	std::string NextShop;
+	std::cout << "When will you next go shopping?: \n";
+	std::cin >> NextShop;
+
+	//lol cba doing this, doubt I will finish tbh. 
+}
